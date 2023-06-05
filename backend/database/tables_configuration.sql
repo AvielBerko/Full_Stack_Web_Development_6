19d339bdfdb352deeefa34803b05c23efea3ff65
chananelz <chananelz@gmail.com>
2023-06-05 10:52:00 +0300

    end create table start create value

--- backend/database/tables_configuration.sql
+++ backend/database/tables_configuration.sql
@@ -1,43 +1,43 @@
 CREATE TABLE `users` (
-  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
   `username` varchar(255),
   `email` varchar(255),
   `company_name` varchar(255),
   `city` varchar(255),
-  `valid` BOOLEAN
+  `valid` BOOLEAN DEFAULT TRUE,
+  UNIQUE (`username`)
 );
 
 CREATE TABLE `todos` (
-  `id` integer PRIMARY KEY,
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
   `userId` integer,
   `title` varchar(255),
-  `completed` BOOLEAN,
-  `valid` BOOLEAN
+  `completed` BOOLEAN DEFAULT FALSE,
+  `valid` BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE `posts` (
-  `id` integer PRIMARY KEY,
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
   `userId` integer,
   `title` varchar(255),
   `body` varchar(255),
-  `valid` BOOLEAN
+  `valid` BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE `comments` (
-  `id` integer PRIMARY KEY,
-  `postId` varchar(255),
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `postId` integer,
   `name` varchar(255),
   `email` varchar(255),
   `body` varchar(255),
-  `valid` BOOLEAN
+  `valid` BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE `user_password` (
-  `id` integer PRIMARY KEY,
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
   `userId` integer,
-  `username` varchar(255),
   `password` varchar(255),
-  `valid` BOOLEAN
+  `valid` BOOLEAN DEFAULT TRUE
 );
 
 ALTER TABLE `todos` ADD FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
