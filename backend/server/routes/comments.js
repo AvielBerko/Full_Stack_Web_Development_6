0d4fed4811eb6d8793d6b7662e740461dac443ff
chananelz <chananelz@gmail.com>
2023-06-06 13:54:49 +0300

    add complex query comments

--- backend/server/routes/comments.js
+++ backend/server/routes/comments.js
@@ -5,6 +5,17 @@ const router = express.Router();
 
 router.use(logger);
 
+router.get("/", (req, res) => {
+  databaseManagement.getEntityByColumn(
+    "comments",
+    "postId",
+    req.query.postId,
+    (result) => {
+      res.send(result);
+    }
+  );
+});
+
 router.get("/:id", (req, res) => {
   databaseManagement.getEntityByColumn(
     "comments",
