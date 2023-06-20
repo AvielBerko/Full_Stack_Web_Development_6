f4449e6441e89b255f219aa3a6ea9d570fc47a54
chananelz <chananelz@gmail.com>
2023-06-20 10:37:58 +0300

    1

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import initJPHGetLoader from "./lib/data/loders/jsonPlaceholder/jsonPlaceHolderGetLoader.ts";
-import initJPHSaveLoader from "./lib/data/loders/jsonPlaceholder/jsonPlaceHoldergetSavers.ts";
+// import initJPHGetLoader from "./lib/data/loders/jsonPlaceholder/jsonPlaceHolderGetLoader.ts";
+// import initJPHSaveLoader from "./lib/data/loders/jsonPlaceholder/jsonPlaceHoldergetSavers.ts";
 import initExpressGetLoader from "./lib/data/loders/express/expressGetLoader.ts";
 import initExpressSaveLoader from "./lib/data/loders/express/expressGetSavers.ts";
 
 //setup database managers (DI container)
-initJPHGetLoader();
-initJPHSaveLoader();
-// initExpressGetLoader();
-// initExpressSaveLoader();
+// initJPHGetLoader();
+// initJPHSaveLoader();
+initExpressGetLoader();
+initExpressSaveLoader();
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
