c0b7ff71a0c842c6fdc1b71f45977217d689b4f6
chananelz <chananelz@gmail.com>
2023-06-21 13:39:23 +0300

    it work 14:00

--- backend/server/express_server.js
+++ backend/server/express_server.js
@@ -3,14 +3,8 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Set-Cookie"],
-    credentials: true,
-  })
-);
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
